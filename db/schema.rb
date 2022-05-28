105538a802d3a1c0820ddb565762ae3e6ddfdad0
yogurt-mr <105791710+yogurt-mr@users.noreply.github.com>
2022-05-28 17:25:56 +0900

    メッセージ送信機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,13 @@
 ActiveRecord::Schema.define(version: 2022_05_27_080051) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.bigint "room_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -45,6 +50,8 @@ ActiveRecord::Schema.define(version: 2022_05_27_080051) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "room_users", "rooms"
   add_foreign_key "room_users", "users"
 end
